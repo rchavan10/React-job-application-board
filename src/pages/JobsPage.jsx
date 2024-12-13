7d8c83f8d2b1c6650986a5dc9df2bac65af3fd34
Raj Chavan <rajchavan@Rajs-MacBook-Air.local>
2024-12-13 12:39:03 -0600

    updated Job Listings page

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import JobListings from '../components/JobListings.jsx'
 
 const JobsPage = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <section className='bg-blue-50 px-4 py-6'>
+      <JobListings/>
+    </section>
   )
 }
 
