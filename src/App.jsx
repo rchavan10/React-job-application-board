5c49c81c4896787e3e88c14916cf73df2ff2f09c
Raj Chavan <rajchavan@Rajs-MacBook-Air.local>
2024-12-13 11:46:18 -0600

    changed a tags to Link from React Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import HomePage from './pages/HomePage'; // instead of components, we will have pages in our App
 import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     // create parent route '/'
     <Route path='/' element={<MainLayout/>}>
       <Route index element={<HomePage/>}/>
+      <Route path='/jobs' element={<JobsPage/>}/>
     </Route>
 )
 );
