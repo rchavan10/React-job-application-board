9632e4f4f08cfe2253af3f966baf8dd2c705d1eb
Raj Chavan <rajchavan@Rajs-MacBook-Air.local>
2024-12-14 20:06:36 -0600

    created POST request

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,16 @@ import AddJobPage from './pages/AddJobPage';
 
 const App = () => {
 
-  const addJob = (newJob) => {
-
-  }
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+    return;
+  };
   
   const router = createBrowserRouter(
     createRoutesFromElements(
